3e162c0e48ccc9e3cef1d44438e0cc3e8181833b
Vittorio <vittorio.cadastro@gmail.com>
2016-05-20 07:22:12 -0300

    Adicionar footer e criar pasta 'app' para armanezar os arquivos angular

--- public/app/produtos_ml/config/produtos_ml.client.routes.js
+++ public/app/produtos_ml/config/produtos_ml.client.routes.js
@@ -3,12 +3,12 @@
  */
 angular.module('produtosml').config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/produtos', {
-        templateUrl: '../produtos_ml/views/list-produtos.client.view.html'
+        templateUrl: 'app/produtos_ml/views/list-produtos.client.view.html'
     }).when('/produtos/create', {
-        templateUrl: '../produtos_ml/views/create-produto.client.view.html'
+        templateUrl: 'app/produtos_ml/views/create-produto.client.view.html'
     }).when('/produtos/:produtoId', {
-        templateUrl: '../produtos_ml/views/view-produto.client.view.html'
+        templateUrl: 'app/produtos_ml/views/view-produto.client.view.html'
     }).otherwise({
-        redirectTo: '/'
+        redirectTo: '/produtos'
     });
 }]);
--- public/app/vendedores_ml/config/vendedores_ml.client.routes.js
+++ public/app/vendedores_ml/config/vendedores_ml.client.routes.js
@@ -3,10 +3,10 @@
  */
 angular.module('vendedoresml').config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/vendedores', {
-        templateUrl: '../vendedores_ml/views/list-vendedores.client.view.html'
+        templateUrl: 'app/vendedores_ml/views/list-vendedores.client.view.html'
     }).when('/vendedores/create', {
-        templateUrl: '../vendedores_ml/views/create-vendedor.client.view.html'
+        templateUrl: 'app/vendedores_ml/views/create-vendedor.client.view.html'
     }).when('/vendedores/:vendedorId', {
-        templateUrl: '../vendedores_ml/views/view-vendedor.client.view.html'
+        templateUrl: 'app/vendedores_ml/views/view-vendedor.client.view.html'
     });
 }]);
