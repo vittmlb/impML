e18924c55993c7e403e3129fdad288e53b370284
Vittorio <vittorio.cadastro@gmail.com>
2016-05-25 23:00:09 -0300

    Adicionados os recursos para sistema de mensagens Toastr (implementado apenas no módulo de produtos)

--- public/app/produtos_ml/controllers/produtos_ml.client.controller.js
+++ public/app/produtos_ml/controllers/produtos_ml.client.controller.js
@@ -1,8 +1,8 @@
 /**
  * Created by Vittorio on 17/05/2016.
  */
-angular.module('produtosml').controller('ProdutosController', ['$scope', '$routeParams', '$location', 'ProdutosML',
-    function($scope, $routeParams, $location, ProdutosML) {
+angular.module('produtosml').controller('ListProdutosController', ['$scope', '$routeParams', '$location', 'ProdutosML', 'ngToast',
+    function($scope, $routeParams, $location, ProdutosML, ngToast) {
         $scope.create = function() {
             var produto = new ProdutosML({
                 produtoId: this.produtoId
@@ -10,46 +10,8 @@ angular.module('produtosml').controller('ProdutosController', ['$scope', '$route
             produto.$save(function (response) {
                 $location.path('/produtos/' + response._id);
             }, function(errorResponse) {
-                $scope.error = errorResponse.data;
-            });
-        };
-        $scope.find = function() {
-            $scope.produtos = ProdutosML.query();
-        };
-        $scope.findOne = function() {
-            $scope.produto = ProdutosML.get({
-                produtoId: $routeParams.produtoId
-            });
-        };
-        $scope.atualizarVenda = function(item) {
-            item.$update(function () {
-                $location.path('/produtos');
-            }, function(errorResponse) {
-                $scope.error = errorResponse;
-            });
-        };
-        $scope.atualizarTodasVendas = function() {
-            $scope.produtos.forEach(function(prod) {
-                prod.$update(function () {
-                    alert('Success');
-                }, function(errorResponse) {
-                    $scope.error = errorResponse.data.message
-                });
-            });
-        };
-    }
-]);
-
-angular.module('produtosml').controller('ListProdutosController', ['$scope', '$routeParams', '$location', 'ProdutosML',
-    function($scope, $routeParams, $location, ProdutosML) {
-        $scope.create = function() {
-            var produto = new ProdutosML({
-                produtoId: this.produtoId
-            });
-            produto.$save(function (response) {
-                $location.path('/produtos/' + response._id);
-            }, function(errorResponse) {
-                $scope.error = errorResponse.data;
+                ngToast.danger(errorResponse.data.message.errmsg);
+                $scope.error = errorResponse.data.message.errmsg;
             });
         };
         $scope.find = function() {
@@ -71,15 +33,15 @@ angular.module('produtosml').controller('ListProdutosController', ['$scope', '$r
         $scope.atualizarTodasVendas = function() {
             $scope.produtos.forEach(function(prod) {
                 prod.$update(function () {
-                    alert('Success');
+                    ngToast.create('Produto atualizado com sucesso !!!');
                 }, function(errorResponse) {
+                    ngToast.create(errorResponse.data.message);
                     $scope.error = errorResponse.data.message
                 });
             });
         };
 
         $scope.numItemsPerPage = 10;
-
-
+        
     }
 ]);
